28ce804289257503d5f4e7e70e6318560feaeb09
krugbuild <stefan.krug@posteo.org>
2022-01-11 17:18:15 +0100

    aggregated array in view

---
+++ create/CREATE_FUNCTION_return_erfasser.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION development.return_erfasser(
+  _objekt_id integer              -- pk
+  ) 
+RETURNS text[] AS $$
+
+DECLARE 
+  _ar text[];
+BEGIN
+
+SELECT ARRAY( 
+  SELECT def.name || '|' || def.id::text || '|' || COALESCE(rel.is_creator::text, '0')
+    FROM development.obj_basis AS obb
+    JOIN development.rel_erfasser AS rel ON rel.ref_objekt_id = obb.objekt_id
+    JOIN development.def_erfasser AS def ON rel.ref_erfasser_id = def.id
+    WHERE obb.objekt_id = _objekt_id)
+INTO _ar;
+
+RETURN _ar;
+
+END;
+$$ LANGUAGE plpgsql;
--- create/CREATE_VIEW_objektbereich_poly.sql
+++ create/CREATE_VIEW_objektbereich_poly.sql
@@ -5,10 +5,8 @@
 
 CREATE OR REPLACE VIEW development.objektbereich_poly AS
 
-SELECT objekt_id, ob.ref_objekt_id, objekt_nr, erfassungsdatum, aenderungsdatum, in_bearbeitung, beschreibung, beschreibung_ergaenzung, lagebeschreibung, quellen_literatur, notiz_intern, hida_nr, stammdaten_id, sachbegriff, bezeichnung, bauwerksname_eigenname, kategorie, erhaltungszustand, schutzstatus, nachnutzungspotential, kreis, gemeinde, ort, sorbisch, strasse, hausnummer, gem_flur, geom
+SELECT objekt_id, ob.ref_objekt_id, objekt_nr, erfassungsdatum, aenderungsdatum, development.return_erfasser(ob.objekt_id), in_bearbeitung, beschreibung, beschreibung_ergaenzung, lagebeschreibung, quellen_literatur, notiz_intern, hida_nr, stammdaten_id, sachbegriff, bezeichnung, bauwerksname_eigenname, kategorie, erhaltungszustand, schutzstatus, nachnutzungspotential, kreis, gemeinde, ort, sorbisch, strasse, hausnummer, gem_flur, geom
 	FROM development.obj_basis AS ob
 	JOIN development.geo_poly AS geo ON ob.objekt_id = geo.ref_objekt_id
 	JOIN development.obj_stamm AS os ON ob.objekt_id = os.ref_objekt_id
-	FULL JOIN (SELECT (ARRAY[[0],[0]]) AS nullcolumn) AS sq ON 0 = ob.objekt_id -- nur als dummy Spalte gedacht
-	WHERE ob.geloescht IS NOT TRUE
-    AND ob.objekt_id IS NOT NULL;
+	WHERE ob.geloescht IS NOT TRUE;
