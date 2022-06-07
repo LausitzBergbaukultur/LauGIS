24b4fde4a533ac9f700a4a31866daed285bf89f5
krugbuild <stefan.krug@posteo.org>
2022-06-07 15:33:22 +0200

    removed prerelease

--- prerelease/updates/UPDATE_20220111.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO "Erfassung"."def_Nachnutzung" (bezeichnung)
-	VALUES ('zur Förderung empfohlen'), ('in Nutzung');
--- prerelease/updates/UPDATE_20220222.sql
+++
@@ -1,7 +0,0 @@
-UPDATE "Erfassung"."def_Kategorie"
-	SET bezeichnung='Kat. 2 - Energieerzeugung'
-	WHERE id = 2;
-	
-UPDATE "Erfassung"."def_Kategorie"
-	SET bezeichnung='Kat. 6 - Begleit- und Folgeindustrie'
-	WHERE id = 6;
